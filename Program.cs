68d54a71b840b0a7d89752c825811c09af7bc4ed
NinetynineFive <casper.jaller@gmail.com>
2020-11-29 16:51:21 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bowling
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
