13a5b0cfbbcaed46151ecd1600bfd5c941971018
NinetynineFive <casper.jaller@gmail.com>
2020-11-30 15:41:23 +0100

    Added BowlingGame

--- Bowling/BowlingGame.cs
+++ Bowling/BowlingGame.cs
@@ -1,10 +1,21 @@
 ﻿using System;
+using BowlingLibrary;
+
 namespace Bowling
 {
     public class BowlingGame
     {
+        
         public BowlingGame()
         {
+            
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Welcome to the Bowling Game!");
+
+            
         }
     }
 }
