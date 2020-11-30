a516467d0c4a574acc9d11a4a03c25d6f738a6fb
NinetynineFive <casper.jaller@gmail.com>
2020-11-30 13:47:01 +0100

    Refactor and added tests

--- BowlingLibrary.UnitTest/RoundTest.cs
+++ BowlingLibrary.UnitTest/RoundTest.cs
@@ -7,8 +7,9 @@ namespace BowlingLibrary.UnitTest
     [TestClass]
     public class RoundTest
     {
+
         [TestMethod]
-        public void Round_ReturnsCorrectAmmount()
+        public void ReturnsCorrectAmmount()
         {
 
             var bowlinground1 = new Round();
@@ -17,6 +18,57 @@ namespace BowlingLibrary.UnitTest
 
 
             Assert.AreEqual(actual, 8);
+        }
+
+        [TestMethod]
+        public void ReturnsNoMorePinsThenZero()
+        {
+
+            var bowlinground1 = new Round();
+
+            var actual = bowlinground1.Roll(-5);
+
+
+            Assert.AreEqual(actual, 0);
+        }
+
+        [TestMethod]
+        public void ReturnsNoMorePinsThenTen()
+        {
+
+            var bowlinground1 = new Round();
+
+            var actual = bowlinground1.Roll(20);
+
+
+            Assert.AreEqual(actual, 10);
+        }
+
+        [TestMethod]
+        public void ReturnsScore()
+        {
+
+            var bowlinground1 = new Round();
+
+            var actual = bowlinground1.Roll(20);
+
+
+            Assert.AreEqual(actual, 10);
+        }
+
+        [TestMethod]
+        public void GutterGame()
+        {
+
+            var bowlinground1 = new Round();
+
+            for (int i = 0; i < 20; i++)
+            {
+                bowlinground1.Roll(0);
+            }
+
+
+            Assert.AreEqual(bowlinground1.Score(), 0);
         }
     }
 }
--- BowlingLibrary/Round.cs
+++ BowlingLibrary/Round.cs
@@ -4,10 +4,26 @@ namespace BowlingLibrary
 {
     public class Round
     {
-        
-        public int Roll(int pins)
+        private int score;
+
+        public Round()
         {
 
+        }
+
+        public int Roll(int pins)
+        {
+            if (pins > 10) { pins = 10; }
+                
+            if (pins < 0) { pins = 0; }
+
+            score += pins;
+            return score;
+        }
+
+        public int Score()
+        {
+            
             return 0;
         }
     }
