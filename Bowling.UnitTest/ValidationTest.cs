57787094edc9841005fc8c90d8405b2509f3490f
NinetynineFive <casper.jaller@gmail.com>
2020-12-01 18:23:44 +0100

    Removed 'Properties' and added Tests to Validation for PlayerSelection

--- Bowling.UnitTest/ValidationTest.cs
+++ Bowling.UnitTest/ValidationTest.cs
@@ -4,7 +4,7 @@ using Bowling;
 namespace Bowling.UnitTest
 {
     [TestClass]
-    public class BowlingTests
+    public class PlayerSelectValidation
     {
         [TestMethod]
         public void PlayerSelectionOnlyAcceptsNumbersOneToFour()
@@ -17,6 +17,66 @@ namespace Bowling.UnitTest
         }
 
         [TestMethod]
+        public void PlayerSelectionOnlyAcceptsNumbersOneToFourOneTest()
+        {
+            var newValidation = new Validation();
+
+            var actual = newValidation.ValidateAmountOfPlayers(1);
+
+            Assert.AreEqual(actual, 1);
+        }
+
+        [TestMethod]
+        public void PlayerSelectionOnlyAcceptsNumbersOneToFourTwoTest()
+        {
+            var newValidation = new Validation();
+
+            var actual = newValidation.ValidateAmountOfPlayers(2);
+
+            Assert.AreEqual(actual, 2);
+        }
+
+        [TestMethod]
+        public void PlayerSelectionOnlyAcceptsNumbersOneToFourThreeTest()
+        {
+            var newValidation = new Validation();
+
+            var actual = newValidation.ValidateAmountOfPlayers(3);
+
+            Assert.AreEqual(actual, 3);
+        }
+
+        [TestMethod]
+        public void PlayerSelectionOnlyAcceptsNumbersOneToFourFourTest()
+        {
+            var newValidation = new Validation();
+
+            var actual = newValidation.ValidateAmountOfPlayers(4);
+
+            Assert.AreEqual(actual, 4);
+        }
+
+        [TestMethod]
+        public void PlayerSelectionReturnsZeroIfUnderOne()
+        {
+            var newValidation = new Validation();
+
+            var actual = newValidation.ValidateAmountOfPlayers(-1);
+
+            Assert.AreEqual(actual, 0);
+        }
+
+        [TestMethod]
+        public void PlayerSelectionReturnsZeroIfOverFour()
+        {
+            var newValidation = new Validation();
+
+            var actual = newValidation.ValidateAmountOfPlayers(5);
+
+            Assert.AreEqual(actual, 0);
+        }
+
+        [TestMethod]
         public void PlayerSelectionRejectsNumbersLessThen1()
         {
             var newValidation = new Validation();
@@ -25,6 +85,7 @@ namespace Bowling.UnitTest
 
             Assert.AreEqual(actual, 0);
         }
+
         [TestMethod]
         public void PlayerSelectionRejectsNumbersMoreThen4()
         {
